34f010066b45b39e218af2b49171a561229e6b60
rndkonst-pixel <rndkonst@gmail.com>
2025-09-16 16:03:56 +0300

    Add files via upload

--- Пр1АнализИСравнениеПростыхАлгоритмов.cpp
+++ Пр1АнализИСравнениеПростыхАлгоритмов.cpp
@@ -3,6 +3,8 @@
 #include <chrono>
 #include <string>
 #include <algorithm>
+#include <unordered_set>
+#include <set>
 
 using namespace std;
 
@@ -65,6 +67,7 @@ bool isPalindrome(string s) {
         left++;
         right--;
     }
+    return true;
 }
 
 bool isPalindrome2(string s) {
@@ -92,6 +95,32 @@ bool isPalindrome2(string s) {
     return true;
 }
 
+int countUniqueSorted(vector<int> arr) {
+    if (arr.empty()) return 0;
+
+    sort(arr.begin(), arr.end());
+    int count = 1;
+
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] != arr[i - 1]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int countUnique(const vector<int>& arr) {
+    set<int> uniqueSet(arr.begin(), arr.end());
+    return uniqueSet.size();
+}
+
+
+int countUnique2(const vector<int>& arr) {
+    unordered_set<int> uniqueSet(arr.begin(), arr.end());
+    return uniqueSet.size();
+}
+
 int main() {
 
     // Устанавливаем локаль
@@ -114,7 +143,7 @@ int main() {
 
     cout << "После пузырьковой сортировки: ";
     printArray(arr1, size);
-    cout << "Время: " << duration1.count() << " микросекунд" << endl;
+    cout << "Время: " << duration1.count() << " наносекунд" << endl;
 
     // Тестируем сортировку вставками
     start = chrono::high_resolution_clock::now();
@@ -124,7 +153,7 @@ int main() {
 
     cout << "После сортировки вставками: ";
     printArray(arr2, size);
-    cout << "Время: " << duration2.count() << " микросекунд" << endl;
+    cout << "Время: " << duration2.count() << " наносекунд" << endl;
 
     string test1 = "A man, a plan, a canal: Panama";
     string test2 = "race a car";
@@ -136,7 +165,7 @@ int main() {
     cout << "\"" << test1 << "\" -> " << isPalindrome(test1) << endl;
     auto end3 = chrono::high_resolution_clock::now();
     auto duration3 = chrono::duration_cast<chrono::nanoseconds>(end3 - start3);
-    cout << "Время isPalindrome: " << duration3.count() << " микросекунд" << endl;
+    cout << "Время isPalindrome: " << duration3.count() << " наносекунд" << endl;
 
     cout << "\"" << test2 << "\" -> " << isPalindrome(test2) << endl;
     cout << "\"" << test3 << "\" -> " << isPalindrome(test3) << endl;
@@ -148,14 +177,36 @@ int main() {
     cout << "\"" << test1 << "\" -> " << isPalindrome2(test1) << endl;
     auto end4 = chrono::high_resolution_clock::now();
     auto duration4 = chrono::duration_cast<chrono::nanoseconds>(end4 - start4);
-    cout << "Время isPalindrome2: " << duration4.count() << " микросекунд" << endl;
+    cout << "Время isPalindrome2: " << duration4.count() << " наносекунд" << endl;
 
     cout << "\"" << test2 << "\" -> " << isPalindrome2(test2) << endl;
     cout << "\"" << test3 << "\" -> " << isPalindrome2(test3) << endl;
     cout << "\"" << test4 << "\" -> " << isPalindrome2(test4) << endl;
     cout << noboolalpha;
 
+    vector<int> arr = { 1, 2, 3, 2, 4, 1, 5, 3, 6 };
 
+    auto start5 = chrono::high_resolution_clock::now();
+    int cntu = countUniqueSorted(arr);
+    auto end5 = chrono::high_resolution_clock::now();
+    auto duration5 = chrono::duration_cast<chrono::nanoseconds>(end5 - start5);
+    cout << "Количество уникальных элементов: " << cntu << endl;
+    cout << "Время countUniqueSorted: " << duration5.count() << " наносекунд" << endl;
+
+
+    auto start6 = chrono::high_resolution_clock::now();
+    int cntu2 = countUnique(arr);
+    auto end6 = chrono::high_resolution_clock::now();
+    auto duration6 = chrono::duration_cast<chrono::nanoseconds>(end6 - start6);
+    cout << "Количество уникальных элементов: " << cntu2 << endl;
+    cout << "Время countUnique: " << duration6.count() << " наносекунд" << endl;
+
+    auto start7 = chrono::high_resolution_clock::now();
+    int cntu3 = countUnique2(arr);
+    auto end7 = chrono::high_resolution_clock::now();
+    auto duration7 = chrono::duration_cast<chrono::nanoseconds>(end7 - start7);
+    cout << "Количество уникальных элементов: " << cntu3 << endl;
+    cout << "Время countUnique: " << duration7.count() << " наносекунд" << endl;
 
     return 0;
 }
